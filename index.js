5c2d2fe87c544180613f3f913b76a821ae89cfb8
Rutvik Patel <aparawala@zuelligpharma.com>
2024-01-19 14:59:57 +0530

    Nessary Express things done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require("path")
+const bodyParser = require("body-parser")
+const router = require("./UserRoutes")
+
+const app = express();
+const PORT = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
+app.use("/api/v1", router)
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is working on port:${PORT}`)
+})
